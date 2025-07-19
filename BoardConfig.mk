1158a630a04e11b330ad2512fd42d392838a2a58
ZorEl212 <yabsirad212@gmail.com>
2025-07-19 08:12:27 -0400

    ginkgo: Add cmdline flags required  to configure Floppy kernel
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,8 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/4744000.sdhci
+BOARD_KERNEL_CMDLINE += legacy_ir_hal=1
+BOARD_KERNEL_CMDLINE += tsinit legacy_timestamp_source=0
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
